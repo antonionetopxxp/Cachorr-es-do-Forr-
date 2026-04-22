9e3c0f1eda5b87219c8a75061ba7fe2009c72331
antonionetopxxp <159489586+antonionetopxxp@users.noreply.github.com>
2026-04-22 18:35:55 -0300

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,22 @@
 
+<script src="wa_bootstrap/js/jquery.min_v-125-.js"></script>
+    <script>var wa$ = jQuery.noConflict()</script>
+    <script src="wa_js/wa_bootstrap_util_v-125-.js"></script>
+    <script src="wa_bootstrap/js/bootstrap.min_v-125-.js"></script>
+    <script src="wa_js/waVariables_en_v-31-.js"></script>
+    <script src="wa_gallery/js/blueimp-gallery.min_v-125-.js"></script>
+    <script src="wa_gallery/js/wa_galleries_v-125-.js"></script>
+    <script src="wa_animate/wow.min_v-125-.js"></script>
+    <script src="wa_menu/wa_menu_v-125-.js"></script>
+    <script src="wa_menu/wa_search_v-125-.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js?render=explicit" async="" defer=""></script>
+    <script src="wa_js/wa_form_v-125-.js"></script>
+    <script src="wa_js/validator.min_v-125-.js"></script>
+    <script src="wa_js/wa_common_v-125-.js"></script>
+    <script src="wa_js/parallax_v-125-.js"></script>
+    <script src="wa_js/lazyload.min_v-125-.js"></script>
+    <script>let wow = new WOW({boxClass:'wafxWow',animateClass: 'animated',offset:0,mobile:true,live:true,});
+    wow.init();
+    let waGridLazyLoadInstance = new LazyLoad({elements_selector: ".lazy"});
+    </script>
+    <script src="wa_js/wa_RCD_v-125-.js"></script>
